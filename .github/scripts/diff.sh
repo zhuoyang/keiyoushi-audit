2a90612827f094e995e01392263016ece42f3763
Teo Zhuo Yang <zhuo.yang@hotmail.com>
2024-04-10 20:58:23 +0800

    fix diff script path and add job summary

--- .github/scripts/diff.sh
+++ .github/scripts/diff.sh
@@ -12,11 +12,15 @@ do
 
   # Find the corresponding APK file in the second directory and its subdirectories
   apk2=$(find "$dir2" -name "$base_apk" -print -quit)
-
+  
+  echo "### Result" >> $GITHUB_STEP_SUMMARY
   # Check if the APK file exists in the second directory
   if [ -n "$apk2" ]; then
     # Run the apkdiff.py script
-    python apkdiff.py "$apk1" "$apk2"
+    python main/.github/scripts/apkdiff.py "$apk1" "$apk2"
+    if [ $? -ne 0 ]; then
+        echo "$base_apk is different from source" >> $GITHUB_STEP_SUMMARY
+    fi
   else
     echo "File $base_apk does not exist in the second directory."
   fi
